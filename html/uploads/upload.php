8260a626ab25464f43bba95ea40a50e5f89a0245
Erwan Martin <erwan.martin@etu.univ-lehavre.fr>
2025-04-29 14:09:19 +0200

    Message pour le log

--- html/uploads/upload.php
+++ html/uploads/upload.php
@@ -13,7 +13,23 @@ if (isset($_FILES['fileInput']) && !empty($_FILES['fileInput']['name'])) {
     $fileCount = count($uploadedFiles['name']);
     for ($i = 0; $i < $fileCount; $i++) {
         $fileName = basename($uploadedFiles['name'][$i]);
+        $fileName = preg_replace("/[^a-zA-Z0-9\-\_\.]/", "", $fileName);  // Sécuriser le nom du fichier
         $targetPath = $uploadDirectory . $fileName;
+
+        // Vérification des erreurs de téléchargement
+        if ($uploadedFiles['error'][$i] !== UPLOAD_ERR_OK) {
+            echo "Erreur lors du téléchargement du fichier $fileName.<br>";
+            continue;
+        }
+
+        // Vérification du type MIME du fichier
+        $fileType = mime_content_type($uploadedFiles['tmp_name'][$i]);
+        $allowedTypes = ['image/jpeg', 'image/png', 'video/mp4', 'audio/mpeg', 'text/plain'];
+
+        if (!in_array($fileType, $allowedTypes)) {
+            echo "Type de fichier non autorisé pour le fichier $fileName.<br>";
+            continue;
+        }
 
         // Déplacer le fichier du répertoire temporaire vers le répertoire de téléchargement
         if (move_uploaded_file($uploadedFiles['tmp_name'][$i], $targetPath)) {
