13d957174c2c25f95f6844a2ccf4f37bdf57ceea
Erwan Martin <erwan.martin@etu.univ-lehavre.fr>
2025-04-29 14:29:50 +0200

    Message pour le log

--- html/uploads/listFiles.php
+++ html/uploads/listFiles.php
@@ -1,13 +1,5 @@
 <?php
-$directory = 'uploads/';  // Dossier où les fichiers sont stockés
-// Vérifie si le répertoire existe
-if (!is_dir($directory)) {
-    echo json_encode([]); // Si le répertoire n'existe pas, on renvoie un tableau vide
-    exit;
-}
-
-$files = array_diff(scandir($directory), array('..', '.'));  // Liste les fichiers, ignore '.' et '..'
-
-// Renvoie les fichiers sous forme de JSON
+$directory = '../uploads/';
+$files = array_diff(scandir($directory), array('.', '..'));
 echo json_encode(array_values($files));
 ?>
